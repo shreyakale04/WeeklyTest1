3431c05f67392e12f457a3ac5eefbd2d798d0958
shreyakale08 <kaleshreya347@gmail.com>
2026-08-22 18:44:33 +0530

    Find Minimum SubArray Sum using Kadane's Algorithm

---
+++ src/MinSubArray.java
@@ -0,0 +1,24 @@
+
+public class MinSubArray {
+
+	public static void main(String[] args) {
+		int[]arr = {-2,1,-3,4,-1,2,1,-5,4};
+		
+		int currentSum = 0;
+		int MinSum = 0;
+		
+		for(int i = 0; i < arr.length; i++){
+			
+			currentSum = currentSum + arr[i];
+			
+			if(currentSum > 0){
+				
+				currentSum = 0;
+			}
+			MinSum = Math.min(MinSum, currentSum);
+		}
+		System.out.println(MinSum);
+
+	}
+
+}
