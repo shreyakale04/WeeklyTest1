ceaaf193e934b1d2851d837e973b60855a228bea
shreyakale08 <kaleshreya347@gmail.com>
2026-08-24 23:33:50 +0530

    Move Zero using Two Pointer Approach

---
+++ src/practice/MoveZero.java
@@ -0,0 +1,32 @@
+package practice;
+
+public class MoveZero {
+
+	public static void main(String[] args) {
+		
+		int[]arr = {0,1,0,3,12};
+		
+		int index = 0;
+		
+		for(int i = 0; i < arr.length; i++){
+			
+			if(arr[i] != 0){
+				
+				arr[index] = arr[i];
+				
+				index++;
+			}
+		}
+		
+		while(index < arr.length){
+			
+			arr[index] = 0;
+			index++;
+		}
+       for(int num : arr){
+    	   
+    	   System.out.print(num + " ");
+       }
+	}
+
+}
