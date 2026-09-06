2aa29ed6b3c2c682961fba221b13c0ab00e27c82
shreyakale08 <kaleshreya347@gmail.com>
2026-09-06 23:01:36 +0530

    Reverse String Using Two Pointer Approach

---
+++ src/practice/RevString.java
@@ -0,0 +1,31 @@
+package practice;
+
+public class RevString {
+	
+	public void reverseString(char[]s){
+		
+		int left = 0;
+		int right = s.length - 1;
+		
+		while(left<right){
+			
+			char temp = s[left];
+			s[left] = s[right];
+			s[right] = temp;
+			
+			left++;
+			right--;
+			
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		char[]s = {'h','e','l','l','o'};
+		RevString rs = new RevString();
+		
+		rs.reverseString(s);
+		System.out.println(s);
+	}
+
+}
